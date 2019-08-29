8bfe34fbe8e99e48b2dcea030e880c53ea092275
vdmsov <vdmsov@yandex.ru>
2019-08-30 01:11:51 +0300

    get_default(), up_link(), down_link()

--- IfacesCtrl.py
+++ IfacesCtrl.py
@@ -7,21 +7,26 @@ import netifaces as ni
 # for test
 import inspect
 
+#-------------------------------------------------------------------------------
+
 class IfacesCtrl:
 
 
-	ifaces = []
-	
+	iface = []
+
+#-------------------------------------------------------------------------------
+
 	def __init__ (self):
 
 		print("=== IfacesCtrl object is created ===\n")
 
-	
+#-------------------------------------------------------------------------------
+
 	def get_interfaces (self):
 
 		self.func_name()
 
-		process = sp.Popen(['ip', 'addr', 'show'], stdout=sp.PIPE)
+		process = sp.Popen(['ip addr show'], stdout=sp.PIPE, shell = True)
 		stdout = process.communicate()[0]
 
 		temp = re.split('[ :\n]+',stdout)
@@ -29,32 +34,45 @@ class IfacesCtrl:
 
 		for n, l in enumerate(temp):
 			if l[0] == '<':
-				self.ifaces.append(temp[n-1])
+				self.iface.append(temp[n-1])
 
-		self.ifaces.remove('lo')
-
-		print('ifaces = ')
-		for i, val in enumerate(self.ifaces):
-			print(str(i) + ': ' + str(val))
+		self.iface.remove('lo')
 		
+#-------------------------------------------------------------------------------
 
-	def get_default (self):
+	def get_default (self, iface_name):
 		self.func_name()
-	
+		
+		process = sp.Popen(['ip route show | grep default | grep ' + str(iface_name)], \
+			stdout=sp.PIPE, shell = True)
+
+		stdout = process.communicate()[0]
+
+		temp = re.split('[ ]+',stdout)
+
+
+		if temp[0] == 'default':
+			return True
+		else:
+			return False
+
+#-------------------------------------------------------------------------------
 
 	def set_default (self):
 		self.func_name()
 	
+#-------------------------------------------------------------------------------
 
 	def get_gateway (self):
 		self.func_name()
 
+#-------------------------------------------------------------------------------
 	
-	def check_ping (self, dev, amount, wait):
+	def check_ping (self, iface_name, amount, wait):
 
 		self.func_name()
 
-		process = sp.Popen(["""ping -I """ + str(dev) + \
+		process = sp.Popen(["""ping -I """ + str(iface_name) + \
 			""" -c""" + str(amount) + \
 			""" -q -nW""" + str(wait) + \
 			""" 8.8.8.8 | grep loss | awk '{print $(NF-4)}' | cut -d"%" -f1"""], \
@@ -62,33 +80,52 @@ class IfacesCtrl:
 
 		packet_loss = process.communicate()[0]
 
-		print('packet loss = ')
-		print(packet_loss)
-		return packet_loss
+		if packet_loss >= 0:
+			return packet_loss
+		else:
+			return 100		
 
+#-------------------------------------------------------------------------------
 	
 	def reboot_network (self):	
 		self.func_name()
 
+#-------------------------------------------------------------------------------
 	
-	def up_link (self):
+	def up_link (self, iface_name):
 		self.func_name()
 
-	
-	def down_link (self):
+		sp.Popen(['ip link set ' + str(iface_name) + ' up'], \
+			stdout=sp.PIPE, shell = True)
+
+		print('Interface is Up', iface_name)
+
+#-------------------------------------------------------------------------------
+
+	def down_link (self, iface_name):
 		self.func_name()
 
-	
+		sp.Popen(['ip link set ' + str(iface_name) + ' down'], \
+			stdout=sp.PIPE, shell = True)
+
+		print('Interface is Down', iface_name)
+
+#-------------------------------------------------------------------------------
+
 	def check_static_ip (self):
 		self.func_name()
 
+#-------------------------------------------------------------------------------
 	
 	def arp_request (self):
 		self.func_name()
 
+#-------------------------------------------------------------------------------
 	
 	def get_arp_gateway (self):
 		self.func_name()
+
+#-------------------------------------------------------------------------------
 
 #======================== FOR TEST =================================
 	
--- test_IfacesCtrl.py
+++ test_IfacesCtrl.py
@@ -5,6 +5,29 @@ from IfacesCtrl import IfacesCtrl
 
 ifaces = IfacesCtrl()
 
+
+
 ifaces.get_interfaces()
 
-packet_loss = ifaces.check_ping(ifaces.ifaces[1], 1, 1)
+print('iface = ')
+for i, val in enumerate(ifaces.iface):
+	print(str(i) + ': ' + str(val))
+	
+
+
+packet_loss = ifaces.check_ping(ifaces.iface[1], 1, 1)
+
+print('packet_loss = ', str(packet_loss))
+
+
+
+default = ifaces.get_default(ifaces.iface[1])
+
+print('default: ', ifaces.iface[1], default)
+
+
+
+ifaces.down_link(ifaces.iface[1])
+
+ifaces.up_link(ifaces.iface[1])
+
